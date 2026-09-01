4fb3a465ef5d2b61eb26a36bd28e4d7f47b4a14d
https://github.com/shad651 <shadbattleground@gmail.com>
2026-09-01 16:02:55 +0530

    Added expo dependencies

---
+++ node_modules/react-native-webview/lib/NativeRNCWebViewModule.d.ts
@@ -0,0 +1,7 @@
+import type { TurboModule } from 'react-native';
+import { Double } from 'react-native/Libraries/Types/CodegenTypes';
+export interface Spec extends TurboModule {
+    shouldStartLoadWithLockIdentifier(shouldStart: boolean, lockIdentifier: Double): void;
+}
+declare const _default: Spec;
+export default _default;
